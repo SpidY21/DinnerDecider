19253b040aef06f3ba58cb05157bdee9ff3a6a3f
SpidY21 <yashkumarroy164@gmail.com>
2023-06-27 00:52:21 +0530

    Final Commit

--- app/src/main/java/com/example/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/example/dinnerdecider/MainActivity.kt
@@ -7,20 +7,15 @@ import android.widget.EditText
 import android.widget.TextView
 import java.util.*
 
-//import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var selectfoodTxt:TextView
-    val foodList= arrayListOf("Indian","Bengali","Burger King","Pizza","Momo","Daal","Roti","Kheer")
+    val foodList= arrayListOf("")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<TextView>(R.id.selectedFoodTxt)
-//        R.id.selectedFoodTxt="Burgur King"
-//        TextView(R.id.selectedFoodTxt)
-//        val textView =findViewById<TextView>(R.id.selectedFoodTxt)
         selectfoodTxt=findViewById(R.id.selectedFoodTxt)
-//        selectfoodTxt.text="Burger King"
         val btn1=findViewById<Button>(R.id.decideBtn)
         btn1.setOnClickListener{
             val random = Random()
